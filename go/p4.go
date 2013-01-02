a7fc8b93f1d828d76346a9ec97fe83cdb9ce4bad
Chris Ventura <cven714@gmail.com>
2013-01-02 13:28:02 -0500

    Completed problem 4

---
+++ go/p4.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	maxPalindrome := 0
+	
+	for i := 999; i > 99; i-- {
+		for j := i; j > 99; j-- {
+			p := i * j
+			
+			if isPalindrome(p) && p > maxPalindrome {
+				maxPalindrome = p
+			}
+		}
+	}
+	
+	fmt.Println(maxPalindrome)
+}
+
+func isPalindrome(n int) bool {
+	s := strconv.Itoa(n)
+	
+	for start, end := 0, len(s) - 1; start <= end; start, end = start + 1, end - 1 {
+		if s[start] != s[end] {
+			return false
+		}
+	}
+	
+	return true
+}
